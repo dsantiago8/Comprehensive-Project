20cc87c03a26e0a0343a979fe0e91fd7894303c3
Diego <dsantiago@oxy.edu>
2024-09-03 16:29:41 -0700

    current_user undefined

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_login import (
+    LoginManager, login_required, current_user, login_user, logout_user)
+
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
